8dcd11c0d372edfaf395c11883b1c667376c6a55
Daniel Rothamel <daniel.rothamel@gmail.com>
2021-09-17 10:54:29 -0400

    changed filename

---
+++ variables.tf
@@ -0,0 +1,71 @@
+variable "region" {
+  description = "The region the environment is going to be installed into"
+  type = string
+  default = "us-east-1"
+}
+
+# VPC variables
+
+variable "vpc_name" {
+  description = "Name of the VPC.  Will be the prefix for all named resources"
+  type = string
+  default     = "LUIT-terraform-vpc"
+}
+
+variable "cidr" {
+  description = "CIDR range of VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "availability zones to be created in the VPC"
+  type = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "private_subnet" {
+  description = "CIDR range for private subnets"
+  type = list(string)
+  default = ["10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "CIDR range for public subnets"
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "enable nat gateway?"
+  type = bool
+  default = true
+}
+
+variable "enable_vpn_gateway" {
+  description = "enable vpn gateway?"
+  type = bool
+  default = true
+}
+
+# Database variables
+
+variable "db_username" {
+  description = "Database administrator username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  type        = string
+  sensitive   = true
+}
+
+# Load Balancer variables
+
+variable "lb_name" {
+    description = "the name of the load balancer"
+    type = string
+    default = "terraform lb"
+}

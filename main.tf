243394ee7650b5bf1375ad40a4ae9073e99b5826
Daniel Rothamel <daniel.rothamel@gmail.com>
2021-09-17 10:50:26 -0400

    First commit of main and variables. Updated readme

---
+++ main.tf
@@ -0,0 +1,95 @@
+provider "aws" {
+  region = var.region
+}
+
+#VPC module
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+# RDS module
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 3.0"
+
+  identifier = "demodb"
+
+  engine            = "mysql"
+  engine_version    = "5.7.19"
+  instance_class    = "db.t2.micro"
+
+  name     = "demodb"
+  username = var.db_username
+  password = var.db_pasword
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = ["sg-12345678"]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval = "30"
+  monitoring_role_name = "MyRDSMonitoringRole"
+  create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB parameter group
+  family = "mysql5.7"
+
+  # DB option group
+  major_engine_version = "5.7"
+
+  # Database Deletion Protection
+  deletion_protection = true
+
+  parameters = [
+    {
+      name = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+}
+
+# Load Balancer module
+
+resource "aws_lb" "test" {
+  name               = var.lb_name
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = aws_subnet.public.*.id
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}

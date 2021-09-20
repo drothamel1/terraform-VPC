241430dfd2471f8ac09455fcc694d6f350fcfb46
Daniel Rothamel <daniel.rothamel@gmail.com>
2021-09-20 11:40:34 -0400

    made changes to lb file and db file

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "LUIT_lb" {
   load_balancer_type = "application"
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     Name = "LUIT_lb"
